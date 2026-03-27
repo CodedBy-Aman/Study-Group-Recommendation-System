8404bb722ae8dc2b8092f87ac179d69aa1803527
mohit-singhkhatri10 <mohitsinghkhatri10@gmail.com>
2026-03-27 23:49:09 +0530

    Update schemas.py

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,12 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel  #pydantic lib is used for data validation
 
-class StudentCreate(BaseModel):
+class StudentCreate(BaseModel):  #this define what user must send example in name i entered Ram then ok but if i entered 0_123 then error
     name: str
     subject: str
     skill_level: int
 
-class StudentResponse(StudentCreate):
+class StudentResponse(StudentCreate):     # thiss defines what backend returns
     id: int
 
     class Config:
         from_attributes = True
+
+#It's purpose is to defines what data is allowed in API request and responses means it checks and control incoming and outgoing data
+#validate users input from frontend and send output to user
