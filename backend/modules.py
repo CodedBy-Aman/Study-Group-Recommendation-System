da335c8016245db929ebc641dd81173f1d9b5960
mohit-singhkhatri10 <mohitsinghkhatri10@gmail.com>
2026-03-28 09:42:13 +0530

    Update modules.py

--- backend/modules.py
+++ backend/modules.py
@@ -1,9 +1,9 @@
 from sqlalchemy import Column, Integer, String  #imort datatypes for table's column means column can store data in terms of integer and text..
-from database import Base #imort batabase from databae.py ...as we are using Base to create Table
+from database import Base #import database from databae.py ...as we are using Base to create Table
 
 class Student(Base):    # creates a class named student this represents a table called  student in database
     __tablename__ = "students"
-                                ## Table student has attribut id ,nam ,subject,skill level in form of column
+                                ## Table student has attribute id ,nam ,subject,skill level in form of column
     id = Column(Integer, primary_key=True, index=True)   
     name = Column(String)
     subject = Column(String)
